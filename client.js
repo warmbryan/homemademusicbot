5a5314a5dc4725ef9f78f66ce23f05671e86e1ae
Bryan Toh <warmbryan@gmail.com>
2023-05-20 19:36:28 +0800

    discordjs breaking changes refactor

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
-const { Client, Intents } = require('discord.js');
+// const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+// const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent] });
 
 module.exports = client;
